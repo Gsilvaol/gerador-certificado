a4de8fa8a43304cda4a7df0e7f5f4ba7ebbecbc4
Gustavo <gustavosilva1994_@hotmail.com>
2025-07-17 14:53:12 -0300

    Usando o Router para Navegação via TypeScript

---
+++ src/app/_components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {
+      this.mensagem();
+  }
+  mensagem(){
+    console.log('Meu componente navbar inicializou(dentro de uma função)');
+  }
+
+ }
