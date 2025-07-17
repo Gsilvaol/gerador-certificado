4d77fda0d961e3938fc4a161f563f367fe1ff486
Gustavo <gustavosilva1994_@hotmail.com>
2025-07-17 16:56:54 -0300

    Usando o NgModel no Formulário

--- src/app/pages/certificados-form/certificados-form.component.ts
+++ src/app/pages/certificados-form/certificados-form.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificados-form',
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FormsModule, CommonModule],
   templateUrl: './certificados-form.component.html',
   styleUrl: './certificados-form.component.css',
 })
-export class CertificadosFormComponent {}
+export class CertificadosFormComponent {
+  nome: string = ''
+  atividade: string = ''
+  atividades: string[] = ['angular', 'react']
+
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched
+  }
+}
